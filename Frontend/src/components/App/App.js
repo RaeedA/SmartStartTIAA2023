6bb679d61a4cf24675e62bacadfd7660c8ba9a79
TherenWhite <124221153+TherenWhite@users.noreply.github.com>
2023-11-03 19:35:29 -0700

    Update App.js to include gamePage Page

--- Frontend/src/components/App/App.js
+++ Frontend/src/components/App/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import Home from '../../pages';
 import Example from '../../pages/example';
+import GamePage from '../../pages/gamePage';
 
 function App() {
     // Contains all the pages in the document and routes to them, more behavior defined in main index
@@ -14,6 +15,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/example' element={<Example />} />
+                <Route path='/game' element={<GamePage />} />
             </Routes>
         </Router>
     );
