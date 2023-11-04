f972d26842b01492cac247b0c85ba117c67d79de
RaeedA <raeedazom@icloud.com>
2023-11-04 16:03:03 -0700

    implementing chatgpt

--- Backend/util.js
+++ Backend/util.js
@@ -1,3 +1,5 @@
+require("dotenv").config(); 
+
 // Helper function that submits a json response with default 200 status
 function finishController(res, message, status = 200) {
     res.status(status).json(message);
