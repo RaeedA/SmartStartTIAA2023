7466f877e90dd1b017a2c74c61627e4fdf2a4a97
RaeedA <raeedazom@icloud.com>
2023-11-04 16:03:03 -0700

    implementing chatgpt

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 const {example} = require('../api/exampleController');
+const { chatGPTRequest } = require('../api/chatGPTController');
 var router = express.Router();
 
 // Main routing page, all routing should be done here
 
 // Post addition example routing
-router.post('/example', function(req, res, next) {
-  example(req, res);
-});
+router.post('/example', example)
+
+router.post('/gptrequest', chatGPTRequest)
 
 module.exports = router;
