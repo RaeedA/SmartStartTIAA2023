1f3d2c865559d43adf82e6a6dd8ccafef424b3a2
TherenWhite <124221153+TherenWhite@users.noreply.github.com>
2023-11-05 02:07:58 -0700

    Update index.js

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,37 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 import title from '../images/title.png';
 
-
-/**
- * Pages index, responsible for the home page
- */
 export default class Index extends React.Component {
-	render () {
-	const Home = () => {
-		return (
-			<div>
-				<h1>It's a page! Hit GamePage for the game!</h1>
-			</div>
-		);
-	};
+  componentDidMount() {
+    document.body.style.overflow = 'hidden';
+  }
 
-	const titleStyle = {
-		backgroundImage: `url(${title})`,
-		backgroundPosition: 'center',
-		backgroundAttachment: 'fixed',
-		backgroundRepeat: 'no-repeat',
-		backgroundColor: 'black',
-		width: '100vw',
-		height: '100vw',
-		
-		};
-		return (
-			<div>
-				<div style={titleStyle}></div>
-			</div>
-			);
-	}
+  componentWillUnmount() {
+    document.body.style.overflow = '';
+  }
+
+  render() {
+    const titleStyle = {
+      backgroundImage: `url(${title})`,
+      backgroundPosition: 'center',
+      backgroundAttachment: 'fixed',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor: 'black',
+      width: '100vw',
+      height: '100vh',
+      overflow: 'hidden',
+      display: 'flex', 
+      flexDirection: 'column',
+      alignItems: 'center',
+	  justifyContent: 'center'
+    };
+
+    const playButtonStyle = {
+      padding: '10px 20px',
+      fontSize: '1.5rem',
+      color: 'white',
+      backgroundColor: '#34eb67',
+      border: 'none',
+      borderRadius: '5px',
+      textDecoration: 'none', 
+      cursor: 'pointer',
+	  marginTop: '600px'
+    };
+
+    return (
+      <div style={titleStyle}>
+        <Link to="/game" style={playButtonStyle}>PLAY</Link> {/* Play button as a link */}
+      </div>
+    );
+  }
 }
-
-
-
