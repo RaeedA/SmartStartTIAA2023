ddbd1bfbe9fc01a014b10099d0365324caac5fdc
TherenWhite <124221153+TherenWhite@users.noreply.github.com>
2023-11-05 02:06:07 -0700

    Update App.js

--- Frontend/src/components/App/App.js
+++ Frontend/src/components/App/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './App.css';
-import Navbar from '../Navbar';
 import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import Home from '../../pages';
@@ -11,7 +10,6 @@ function App() {
     // Contains all the pages in the document and routes to them, more behavior defined in main index
     return (
         <Router>
-            <Navbar />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/example' element={<Example />} />
