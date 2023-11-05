8a055e3bfabe3573b0ded0a5a6e1fcfedee3b596
Ananyaappl3 <145163387+Ananyaappl3@users.noreply.github.com>
2023-11-04 18:17:42 -0700

    Home Page has the Smart Start Logo

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import title from '../images/title.png';
+
 
 /**
  * Pages index, responsible for the home page
  */
-const Home = () => {
-	return (
-		<div>
-			<h1>It's a page! Hit example at the top for the example.</h1>
-		</div>
-	);
-};
+export default class Index extends React.Component {
+	render () {
+	const Home = () => {
+		return (
+			<div>
+				<h1>It's a page! Hit GamePage for the game!</h1>
+			</div>
+		);
+	};
 
-export default Home;
+	const titleStyle = {
+		backgroundImage: `url(${title})`,
+		backgroundPosition: 'center',
+		backgroundAttachment: 'fixed',
+		backgroundRepeat: 'no-repeat',
+		backgroundColor: 'black',
+		width: '100vw',
+		height: '100vw',
+		
+		};
+		return (
+			<div>
+				<div style={titleStyle}></div>
+			</div>
+			);
+	}
+}
+
+
+
